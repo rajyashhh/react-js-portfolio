268f7658f3189e9980e028d7d4fe531bb034d973
Yash Raj Singh <workmail.yrs@gmail.com>
2025-07-05 03:11:01 +0530

    Navbar completed with motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './sections/navbar'
 
 function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
-      {/* navbar */ }
+      <Navbar/>
       {/* hero */ }
       {/* about */ }
       {/* projects */ }
