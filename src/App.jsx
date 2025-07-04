707cfe4e8cd581cce1b3941792693974fde21be1
Yash Raj Singh <workmail.yrs@gmail.com>
2025-07-05 02:17:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      {/* navbar */ }
+      {/* hero */ }
+      {/* about */ }
+      {/* projects */ }
+      {/* experience */ }
+      {/* testimonials */ }
+      {/* contact */ }
+      {/* footer */ }
+    </div>
+  )
+}
+
+export default App
