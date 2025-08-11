fba878aecfcc0f9e289acad30b43245d3db10401
Yash Raj Singh <workmail.yrs@gmail.com>
2025-08-11 23:50:43 +0530

    projects section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
+import Projects from './sections/Projects'
 function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
       <Navbar/>
       <Hero />
       <About/>
+      <Projects/>
       <section className='min-h-screen'/>
       <section className='min-h-screen'/>
       <section className='min-h-screen'/>
@@ -17,7 +19,6 @@ function App() {
       <section className='min-h-screen'/>
       <section className='min-h-screen'/>
       
-      {/* projects */ }
       {/* experience */ }
       {/* testimonials */ }
       {/* contact */ }
