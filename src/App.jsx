966a9ccbae015bf180c77bc59377ac4a83c2f6eb
Yash Raj Singh <workmail.yrs@gmail.com>
2025-08-12 16:53:24 +0530

    build error fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
