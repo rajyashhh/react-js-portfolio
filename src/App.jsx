0fd5f3a385c68b314b65391ef0d0d5aee9f585dc
Yash Raj Singh <workmail.yrs@gmail.com>
2025-08-17 23:52:48 +0530

    vercel speed test added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import Experiences from './sections/Experiences'
 function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
@@ -11,19 +13,13 @@ function App() {
       <Hero />
       <About/>
       <Projects/>
+      <Experiences/>
       <section className='min-h-screen'/>
       <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      <section className='min-h-screen'/>
-      
-      {/* experience */ }
       {/* testimonials */ }
       {/* contact */ }
       {/* footer */ }
+      <SpeedInsights/>
       <Analytics/>
     </div>
   )
